25f0f99cbad9c5aeace5e49247094d164cb1322d
Arjunagi A Rehman <abdul123arj@gmail.com>
2023-09-07 18:49:38 +0530

    I have done both the assignment in same code please refer commit 1 for the assignment 1 or you can uncommit the code from main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,18 @@ import org.json.JSONObject;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-        ApiHandler api1=new ApiHandler("https://api.chucknorris.io/jokes/random");
-        JSONObject out1= api1.callApi();
-        System.out.println(out1);
-        for(var i:out1.keySet()){
-            System.out.println(i+":"+out1.get(i));
+//        ApiHandler api1=new ApiHandler("https://api.chucknorris.io/jokes/random");
+//        JSONObject out1= api1.callApi();
+//        System.out.println(out1);
+//        for(var i:out1.keySet()){
+//            System.out.println(i+":"+out1.get(i));
+//
+//        }
+        ApiHandler api2=new ApiHandler("https://api.zippopotam.us/us/33162");
+        JSONObject out2= api2.callApi();
+        System.out.println(out2);
+        for(var i:out2.keySet()){
+            System.out.println(i+":"+out2.get(i));
 
         }
 
